17fe19abd3b8422d01570983fdca547054b047de
lihw <1914526816@qq.com>
2021-11-26 16:32:02 +0800

    2、完成 Sentinel 服务熔断降级、限流 学习

--- cloud-alibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/SentinelServiceMain8401.java
+++ cloud-alibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/SentinelServiceMain8401.java
@@ -14,6 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class SentinelServiceMain8401 {
     public static void main(String[] args) {
         SpringApplication.run(SentinelServiceMain8401.class, args);
