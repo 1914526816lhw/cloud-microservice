a0654a5d48607e9cd03d4caf6fe196b67a6286bd
lihw <1914526816@qq.com>
2022-08-18 07:45:26 +0800

    测试定时任务

--- cloud-test-service/src/main/java/com/atguigu/TestApplication.java
+++ cloud-test-service/src/main/java/com/atguigu/TestApplication.java
@@ -1,9 +1,15 @@
 package com.atguigu;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author lihw
@@ -14,7 +20,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @MapperScan("com.atguigu.mapper")
 @EnableScheduling
+@EnableAsync
 public class TestApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
     }
--- cloud-test-service/src/main/java/com/atguigu/task/ReadNumTask.java
+++ cloud-test-service/src/main/java/com/atguigu/task/ReadNumTask.java
@@ -6,15 +6,14 @@ import cn.hutool.http.HttpUtil;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author lihw
@@ -23,6 +22,7 @@ import java.util.concurrent.ThreadPoolExecutor;
  * @description
  */
 
+@Slf4j
 @Service
 public class ReadNumTask {
 
@@ -32,9 +32,30 @@ public class ReadNumTask {
 
     private List<String> articleUrls;
 
+    private List<String> listUrl = new ArrayList<>();
+
+    {
+        //商品
+        String url1 = "https://item.taobao.com/item.htm?spm=a1z10.3-c.w4002-24358234104.9.6df267c9Bpy0RB&id=678926529946";
+        String url2 = "https://item.taobao.com/item.htm?spm=a1z10.3-c.w4002-24358234104.11.6dfa67c92ecjVs&id=679221562703";
+        String url3 = "https://item.taobao.com/item.htm?spm=a1z10.3-c.w4002-24358234104.13.6dfa67c92ecjVs&id=679274646253";
+        String url4 = "https://item.taobao.com/item.htm?spm=a1z10.3-c.w4002-24358234104.15.6dfa67c92ecjVs&id=679546451602";
+        String url5 = "https://item.taobao.com/item.htm?spm=a1z10.3-c.w4002-24358234104.17.6dfa67c92ecjVs&id=679614307710";
+        //进店
+        String shopUrl = "https://shop275216655.taobao.com/?spm=2013.1.1000126.d21.cea45ef9bZ4low";
+
+        listUrl.add(url1);
+        listUrl.add(url2);
+        listUrl.add(url3);
+        listUrl.add(url4);
+        listUrl.add(url5);
+        listUrl.add(shopUrl);
+    }
+
 
     /*定时访问：n 秒一次*/
-    @Scheduled(cron = "0/20 * * * * ?")
+    @Async
+    @Scheduled(cron = "0/18 * * * * ?")
     public void ScheduledAccess() {
         if (flag) {
             flag = false;
@@ -44,10 +65,17 @@ public class ReadNumTask {
             }
             for (int i = 0; i < articleUrls.size(); i++) {
                 String url = articleUrls.get(i);
-                HttpRequest request = HttpUtil.createGet(url);
-                request.execute();
+                try {
+                    HttpRequest request = HttpUtil.createGet(url);
+                    request.execute();
+                    //避免请求太快，睡眠 1 秒
+                    Thread.sleep(TimeUnit.SECONDS.toSeconds(1));
+                    log.info("请求地址【{}】成功", url);
+                } catch (Exception e) {
+                    log.error("请求地址【{}】失败,报错信息为：{}", url, e.getMessage());
+                }
             }
-            System.out.println("第" + (total) + "次调用成功");
+            System.out.println("第" + (total) + "次定时任务成功");
             flag = true;
         } else {
             System.out.println("上一次任务还没执行完成");
@@ -69,7 +97,7 @@ public class ReadNumTask {
             HttpResponse response = request.execute();
             JSONObject resJson = JSON.parseObject(response.body());
             JSONArray articleArray = resJson.getJSONObject("data").getJSONArray("list");
-            if (articleArray.size() == 0) {
+            if (articleArray.isEmpty()) {
                 break;
             } else {
                 for (int i = 0; i < articleArray.size(); i++) {
@@ -81,5 +109,31 @@ public class ReadNumTask {
             }
         } while (true);
         return articleUrlList;
+    }
+
+
+    /*刷访问量淘宝*/
+    @Scheduled(cron = "0/3 * * * * ?")
+    @Async
+    public void taobaoAccess() {
+
+        if (flag) {
+            flag = false;
+            total++;
+            for (int i = 0; i < listUrl.size(); i++) {
+                String url = listUrl.get(i);
+                try {
+                    HttpRequest request = HttpUtil.createGet(url);
+                    request.execute();
+                    log.info("请求【淘宝】地址【{}】成功", url);
+                } catch (Exception e) {
+                    log.error("请求【淘宝】地址【{}】失败,报错信息为：{}", url, e.getMessage());
+                }
+            }
+            System.out.println("【淘宝】第" + (total) + "次定时任务成功");
+            flag = true;
+        } else {
+            System.out.println("上一次【淘宝刷访问量】任务还没执行完成");
+        }
     }
 }
